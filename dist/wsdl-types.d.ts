550bf5e76aed5ca6cc9fdfcffb16c55e1085e720
Federico Feroldi <federico@teamdigitale.governo.it>
2018-04-03 16:39:36 +0200

    1.3.0

--- dist/wsdl-types.d.ts
+++ dist/wsdl-types.d.ts
@@ -1,5 +1,8 @@
+/**
+ * Define some aliases for types used in WSDLs
+ */
 export declare type base64Binary = string;
-export declare type dateTime = Date;
-export declare type date = Date;
+export declare type dateTime = string;
+export declare type date = string;
 export declare type gYear = number;
 export declare type int = number;
--- dist/wsdl-types.js
+++ dist/wsdl-types.js
@@ -1,3 +1,6 @@
 "use strict";
+/**
+ * Define some aliases for types used in WSDLs
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 //# sourceMappingURL=wsdl-types.js.map
