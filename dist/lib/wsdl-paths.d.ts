27feb4e810597ae927ca3d947b76c5b050574184
Deeemi <davidedimeco@gmail.com>
2018-06-22 18:37:15 +0200

    Build

---
+++ dist/lib/wsdl-paths.d.ts
@@ -0,0 +1,6 @@
+export declare const FespPspService_WSDL_PATH: string;
+export declare const AvvisiDigitaliService_WSDL_PATH: string;
+export declare const PagamentiTelematiciPspNodoService_WSDL_PATH: string;
+export declare const IscrizioniAvvisaturaService_WSDL_PATH: string;
+export declare const NodoChiediElencoAvvisiDigitaliService_WSDL_PATH: string;
+export declare const FespCdService_WSDL_PATH: string;
