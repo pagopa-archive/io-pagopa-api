e086a2567e67c30c63ed4cbe1ad97dd64bb9b579
Deeemi <davidedimeco@gmail.com>
2018-06-22 10:31:07 +0200

    Added yams-gen to generate yaml files

---
+++ dist/lib/yaml-gen.d.ts
@@ -0,0 +1,4 @@
+declare const apiconnWsdl: any;
+declare const yaml: any;
+declare const fs: any;
+declare var promise: any;
---
+++ dist/lib/yaml-gen.js
@@ -0,0 +1,24 @@
+"use strict";
+const apiconnWsdl = require("apiconnect-wsdl");
+const yaml = require("js-yaml");
+const fs = require("fs");
+var promise = apiconnWsdl.getJsonForWSDL("./wsdl/nodo/CdPerNodo.wsdl");
+promise.then(function (wsdls) {
+    // Get Services from all parsed WSDLs
+    var serviceData = apiconnWsdl.getWSDLServices(wsdls);
+    // Loop through all services and genereate yaml file
+    for (var item in serviceData.services) {
+        var serviceName = serviceData.services[item].service;
+        var wsdlId = serviceData.services[item].filename;
+        var wsdlEntry = apiconnWsdl.findWSDLForServiceName(wsdls, serviceName);
+        var swagger = apiconnWsdl.getSwaggerForService(wsdlEntry, serviceName, wsdlId);
+        fs.writeFile("./yaml-file/" + serviceName + ".yaml", yaml.safeDump(swagger), (err) => {
+            if (err)
+                throw err;
+            console.log('The file has been saved!');
+        });
+    }
+}, function (error) {
+    console.log(error.message);
+});
+//# sourceMappingURL=yaml-gen.js.map
