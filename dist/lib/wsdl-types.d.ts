f8033e70ea5b189fad1c1cb4c1051d889f1258dd
Federico Feroldi <federico@teamdigitale.governo.it>
2018-06-27 15:37:19 +0200

    Removes dist from repo

--- dist/lib/wsdl-types.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Define some aliases for types used in WSDLs
- */
-export declare type base64Binary = string;
-export declare type dateTime = string;
-export declare type date = string;
-export declare type gYear = number;
-export declare type int = number;
