db5b11e5e0a0f778a096a40f8ec4278e7650d932
LsKoder <35400947+LsKoder@users.noreply.github.com>
2018-06-29 15:55:15 +0200

    [#157910857] Endpoint for FespCdService (#42)
    * Implement attachFespCdServer

--- lib/servers.ts
+++ lib/servers.ts
@@ -3,61 +3,32 @@ import * as soap from "soap";
 import * as wsdl_paths from "./wsdl-paths";
 import { readWsdl } from "./utils";
 
-// import interface for "PSP" services
-import * as FespPspService from "../wsdl-lib/FespPspService/PSPPort";
-import * as AvvisiDigitaliService from "../wsdl-lib/AvvisiDigitaliService/PPTPort";
-
-export { FespPspService };
-export { AvvisiDigitaliService };
+// import interface for "CD" services
+import * as FespCdService from "../wsdl-lib/FespCdService/FespCdPortType";
+export { FespCdService };
 
 /**
- * Creates a server for the FespPsp service.
+ * Creates a server for the FespCD service.
  *
  * This function mostly wraps the `soap.listen(...)` method.
  * @see https://github.com/vpulim/node-soap#soaplistenserver-path-services-wsdl---create-a-new-soap-server-that-listens-on-path-and-provides-services
  *
  * @param server    See soap.listen documentation
  * @param path      See soap.listen documentation
- * @param fespPspHandlers  An object that implements the IPSPPortSoap interface
+ * @param fespCdHandlers  An object that implements the IFespCdPortTypeSoap interface
  */
-export async function attachFespPspServer(
+export async function attachFespCdServer(
   server: any,
   path: string,
-  fespPspHandlers: FespPspService.IPSPPortSoap
+  fespCdHandlers: FespCdService.IFespCdPortTypeSoap
 ): Promise<soap.Server> {
-  const wsdl = await readWsdl(wsdl_paths.FespPspService_WSDL_PATH);
+  const wsdl = await readWsdl(wsdl_paths.FespCdService_WSDL_PATH);
 
   const service = {
-    FespPspService: {
-      PSPPort: fespPspHandlers
+    FespCdService: {
+      FespCdPortType: fespCdHandlers
     }
-  };
-
-  return soap.listen(server, path, service, wsdl);
-}
-
-/**
- * Creates a server for the AvvisiDigitali service.
- *
- * This function mostly wraps the `soap.listen(...)` method.
- * @see https://github.com/vpulim/node-soap#soaplistenserver-path-services-wsdl---create-a-new-soap-server-that-listens-on-path-and-provides-services
- *
- * @param server    See soap.listen documentation
- * @param path      See soap.listen documentation
- * @param fespPspHandlers  An object that implements the IPSPPortSoap interface
- */
-export async function attachAvvisiDigitaliServer(
-  server: any,
-  path: string,
-  avvisiDigitaliHandlers: AvvisiDigitaliService.IPPTPortSoap
-): Promise<soap.Server> {
-  const wsdl = await readWsdl(wsdl_paths.FespPspService_WSDL_PATH);
-
-  const service = {
-    AvvisiDigitaliService: {
-      PPTPort: avvisiDigitaliHandlers
-    }
-  };
+  }; 
 
   return soap.listen(server, path, service, wsdl);
 }
