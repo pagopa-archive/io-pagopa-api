d836def47a08ed328077c0c21bd2ace6ef779043
Sergio Manetta <sergiomanetta@yahoo.it>
2018-06-29 15:34:58 +0200

    Revert "Add dist folder to repository"
    This reverts commit 4726c578fede53b49bc34246ad8a4c0bbe3dc6e2.

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as clients from "./lib/clients";
-import * as servers from "./lib/servers";
-export { clients };
-export { servers };
--- dist/wsdl-lib/FespCdService/FespCdPortType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Esito;
-(function (Esito) {
-    Esito["OK"] = "OK";
-    Esito["KO"] = "KO";
-})(Esito = exports.Esito || (exports.Esito = {}));
-exports.envelopeKey = "soapenv";
-//# sourceMappingURL=FespCdPortType.js.map
